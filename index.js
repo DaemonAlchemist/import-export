51c828ef60d0229f46b9b7930a93485e6c319a0a
Andrea <andrea@wittrock.us>
2017-08-26 02:01:17 -0500

    Add extra syntax support

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var hook = require('node-hook');
 
+const importAll = (all, $1, $2) => {
+  return $1.split(",")
+      .map(part => 'var ' + part + '= require("' + $2 + '").' + part.trim() + ';')
+      .join('');
+};
+
 hook.hook('.js', (src, name) => {
   src = src.replace(/import ([^{]*?) from '(.*?)'/g, 'const $1 = require("$2")');
   src = src.replace(/export default ([^ ]*)/g, 'module.exports = $1');
   src = src.replace(/export (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.$2');
   src = src.replace(/export function ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = function');
   src = src.replace(/export class ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = class');
-  src = src.replace(/import {(.*?)} from '(.*?)'/g, (all, $1, $2) => {
+  src = src.replace(/import {(.*?)} from '(.*?)'/g, importAll);
+  src = src.replace(/import {(.*?)} from "(.*?)"/g, importAll);
+  src = src.replace(/export {(.*)}/g, (all, $1) => {
     return $1.split(",")
-      .map(part => 'var ' + part + '= require("' + $2 + '").' + part.trim() + ';')
-      .join('');
+        .map(part => 'module.exports.' + part + ' = ' + part + ';')
+        .join('');
   });
   return src;
 });
